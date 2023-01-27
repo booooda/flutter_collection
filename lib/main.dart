913a7c47fa2c9f0a3e0679accebb64eac9cde6e8
abdo1200 <abdosobhy1200@gmail.com>
2023-01-27 19:36:24 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_collection/home.dart';
 import 'package:flutter_collection/smart%20switch/provider.dart';
-
 import 'package:provider/provider.dart';
-
 import 'duck/duck.dart';
 
 void main() {
